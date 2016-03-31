8f7e9666d77073b1fe1f2654805cce6cf9728003
hwk6991 <khanha@umich.edu>
2016-03-31 17:56:04 -0400

    Added the classes file

---
+++ IceCreamInventoryManagement/IceCreamInventoryManagement/ourClasses.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceCreamInventoryManagement
+{
+    class ourClasses
+    {
+        class City
+        {
+            string cityLabel;
+            string city;
+            string state;
+        }
+
+        class Route
+        {
+            string routeNumber;
+        }
+
+        class Truck
+        {
+            string truckNumber;
+        }
+
+        class Product
+        {
+            string itemNumber;
+            int price;
+        }
+
+        class Driver
+        {
+            string driverId;
+        }
+    }
+}
