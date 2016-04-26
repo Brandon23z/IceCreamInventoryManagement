98d7acad469dbe747b59a3284abadc313669c695
SP33 <xspenc3r@gmail.com>
2016-04-26 17:44:57 -0400

    changes

--- IceCreamInventoryManagement/IceCreamInventoryManagement/autoOrderForm.cs
+++ IceCreamInventoryManagement/IceCreamInventoryManagement/autoOrderForm.cs
@@ -22,6 +22,7 @@ namespace IceCreamInventoryManagement
 
         private void autoOrderForm_Load(object sender, EventArgs e)
         {
+            dataGridView1.Rows.Clear();
             SalesSpecifics specs = new SalesSpecifics();
 
 
@@ -30,33 +31,32 @@ namespace IceCreamInventoryManagement
             specs.date1 = daySettings.currentDate.ToString();
 
             specs.datetype = 1;
+            specs.driver = false;
+            specs.route = false;
+            specs.truck = false;
+            specs.item = false;
 
             List<Sale> mySales = new List<Sale>();
             mySales = getAllSalesSpecific(specs);
 
             for (int i = 0; i < mySales.Count(); i++)
             {
-                DataGridViewRow row = (DataGridViewRow)dataGridView1.Rows[0].Clone();
-                row.Cells[0].Value = mySales[i].itemnumber;
-                row.Cells[1].Value = mySales[i].quantity;
-                dataGridView1.Rows.Add(row);
+                dataGridView1.Rows.Add(mySales[i].itemnumber, mySales[i].quantity);
             }
 
-            List<Sale> mySales1 = new List<Sale>();
-            mySales1 = getAllSales();
+
+            List<InventoryItem> myInventory1 = new List<InventoryItem>();
+            myInventory1 = getInventory();
 
             Settings.NotificationSettings settings = Settings.getNotificationSettings();
 
             int quantitySuggested = settings.setDefaultQuantity;
 
-            for (int i = 0; i < mySales1.Count(); i++)
+            for (int i = 0; i < myInventory1.Count(); i++)
             {
-                if (mySales1[i].quantity == 0 && mySales1[i].saleprice == 0 && mySales1[i].initialprice == 0)
+                if (myInventory1[i].quantity == 0 && myInventory1[i].saleprice == 0 && myInventory1[i].initialprice == 0)
                 {
-                    DataGridViewRow row = (DataGridViewRow)dataGridView1.Rows[0].Clone();
-                    row.Cells[0].Value = mySales1[i].itemnumber;
-                    row.Cells[1].Value = quantitySuggested;
-                    dataGridView1.Rows.Add(row);
+                    dataGridView1.Rows.Add(myInventory1[i].itemnumber, quantitySuggested);
                 }
             }
         }
